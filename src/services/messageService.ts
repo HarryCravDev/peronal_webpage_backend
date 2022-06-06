0ec96f810f8f162525017d0f0eb22f138844b5ef
Harry Craven <harrycraven.dev@gmail.com>
2022-06-06 16:31:42 +0300

    feature/adding-create-and-get-messages

--- src/services/messageService.ts
+++ src/services/messageService.ts
@@ -2,32 +2,45 @@
 import { sendEmail } from "../utils/email/email";
 import { v4 as uuidv4 } from "uuid";
 import { Message } from "../entity/message.entity";
+// import { any } from "src/types/any";
+import { messageData } from "../data/message-data/message-data";
+import { IMessage } from "src/types/IMessage";
 
 export class MessageService {
-	async createMessage(
-		messageObj: any
-	): Promise<{ success: boolean; message: string; data?: any }> {
-		// public createMessage(messageObj: MessageDto): Promise<{ success: boolean; message: string; }> {
+  async createMessage(messageObj: any): Promise<any> {
+    if (!messageObj) {
+      return { success: false, message: "Message successfully sent." };
+    }
 
-		if (!messageObj) {
-			return { success: false, message: "Message successfully sent." };
-		}
+    // Apply id to record
+    messageObj._id = uuidv4();
 
-		// Apply id to record
-		messageObj._id = uuidv4();
+    try {
+      const res = await Message.create(messageObj);
 
-		try {
-			const res = await Message.create(messageObj);
+      sendEmail(messageObj.email, messageObj.subject, messageObj.message);
 
-			sendEmail(messageObj.email, messageObj.subject, messageObj.message);
+      return {
+        success: true,
+        message: "Message successfully sent.",
+        data: res,
+      };
+    } catch (error) {
+      return { success: false, message: error as string };
+    }
+  }
 
-			return {
-				success: true,
-				message: "Message successfully sent.",
-				data: res,
-			};
-		} catch (error) {
-			return { success: false, message: error as string };
-		}
-	}
+  async getMessages(): Promise<any> {
+    try {
+      const res: IMessage[] = messageData;
+
+      return {
+        success: true,
+        message: "Message successfully sent.",
+        data: res,
+      };
+    } catch (error) {
+      return { success: false, message: error as string };
+    }
+  }
 }
