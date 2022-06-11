d8b27efd36d7ed81c1c19ed48f63da293da5643f
Harry Craven <harrycraven.dev@gmail.com>
2022-06-11 22:35:58 +0100

    feature/adding-getMessages-functionality

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -1,17 +1,42 @@
 // src/users/usersController.ts
-import { Body, Controller, Post, Route, SuccessResponse } from "tsoa";
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Route,
+  SuccessResponse,
+  Security,
+} from "tsoa";
 import { MessageService } from "../services/messageService";
 import { MessageDto } from "../dtos/message.dto";
+import { IGenericResponse } from "../types/IGenericResponse";
 
 @Route("message")
 export class MessageController extends Controller {
-	@SuccessResponse("201", "Created") // Custom success response
-	@Post()
-	public async createMessage(@Body() requestBody: MessageDto): Promise<any> {
-		this.setStatus(201); // set return status 201
+  @SuccessResponse("201", "Created") // Custom success response
+  @Post()
+  public async createMessage(
+    @Body() requestBody: MessageDto
+  ): Promise<IGenericResponse> {
+    this.setStatus(201); // set return status 201
 
-		const res = await new MessageService().createMessage(requestBody);
+    const res = await new MessageService().createMessage(requestBody);
 
-		return res;
-	}
+    return res;
+  }
+
+  @Security("jwt")
+  @SuccessResponse("201", "Get messages")
+  @Get()
+  public async getMessages(): Promise<IGenericResponse> {
+    this.setStatus(201);
+    try {
+      const res = await new MessageService().getMessages();
+
+      return res;
+    } catch (error: any) {
+      return { success: false, message: error.message };
+    }
+  }
 }
