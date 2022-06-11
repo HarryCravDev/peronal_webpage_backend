2deacc558190fdc4fa6b210deae9060b5411284c
Harry Craven <harrycraven.dev@gmail.com>
2022-06-11 22:37:37 +0100

    Updating error handling cases.

--- src/utils/errorHandling/errorHandler.util.ts
+++ src/utils/errorHandling/errorHandler.util.ts
@@ -3,33 +3,46 @@ import { AuthenticationError } from "../authentication/authentication";
 import { ValidateError } from "tsoa";
 
 export function errorHandler(
-	err: any,
-	req: express.Request,
-	res: express.Response,
-	next: express.NextFunction
+  err: any,
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
 ): express.Response | void {
-	if (err instanceof AuthenticationError) {
-		console.log(err);
-		return res.status(401).json({
-			error: err.name,
-			message: err.message,
-		});
-	}
+  if (err instanceof AuthenticationError) {
+    console.log(err);
+    return res.status(401).json({
+      error: err.name,
+      message: err.message,
+    });
+  }
 
-	if (err instanceof ValidateError) {
-		console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
-		return res.status(422).json({
-			message: "Validation Failed",
-			details: err?.fields,
-		});
-	}
+  if (err instanceof ValidateError) {
+    console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+    return res.status(422).json({
+      message: "Validation Failed",
+      details: err?.fields,
+    });
+  }
 
-	if (err instanceof Error) {
-		console.log(err);
-		return res.status(500).json({
-			error: err.name,
-			message: err.message,
-		});
-	}
-	next();
+  if (err.message.includes("No token provided")) {
+    return res.status(401).json({
+      error: err.name,
+      message: err.message,
+    });
+  }
+
+  if (err.message.includes("jwt must be provided")) {
+    return res.status(401).json({
+      error: err.name,
+      message: "Incorrect token provided.",
+    });
+  }
+
+  if (err instanceof Error) {
+    return res.status(500).json({
+      error: err.name,
+      message: err.message,
+    });
+  }
+  next();
 }
